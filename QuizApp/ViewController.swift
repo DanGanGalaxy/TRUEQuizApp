e08b6585736dad5c77906eb5f07b941ba3a4717b
DanGanGalaxy <ddavies@unomaha.edu>
2020-07-17 12:28:19 -0500

    did more UI stuff

--- QuizApp/ViewController.swift
+++ QuizApp/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  
+    @IBOutlet weak var circleView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        circleView.layer.cornerRadius = circleView.frame.size.height / 2
+        print("hi")
         // Do any additional setup after loading the view.
     }
 
