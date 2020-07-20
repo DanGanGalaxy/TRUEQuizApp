a199445a06a03997dce950152863c2cf005c399a
DanGanGalaxy <ddavies@unomaha.edu>
2020-07-20 12:18:36 -0500

    set up Sign Up page with firebase stuff and added constraints to it. Also changed segue into signup page to fullscreen.

---
+++ QuizApp/SignUpViewController.swift
@@ -0,0 +1,71 @@
+//
+//  SignUpViewController.swift
+//  QuizApp
+//
+//  Created by Toluwa Davies on 7/20/20.
+//  Copyright © 2020 Toluwa Davies. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class SignUpViewController: UIViewController, UITextFieldDelegate{
+
+    @IBOutlet weak var nameBox: UITextField!
+    @IBOutlet weak var emailBox: UITextField!
+    @IBOutlet weak var passwordBox: UITextField!
+    
+    var name = ""
+    var email = ""
+    var password = ""
+    
+    @IBAction func doneTypingName(_ sender: Any) {
+        name = nameBox.text!
+    }
+    @IBAction func doneTypingEmail(_ sender: Any) {
+        email = emailBox.text!
+    }
+    @IBAction func doneTypingPassword(_ sender: Any) {
+        password = passwordBox.text!
+    }
+    
+    var handle = Auth.auth().addStateDidChangeListener { (auth, user) in
+      // ...
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func signUpButtonPressed(_ sender: Any) {
+        if password != "" && email != "" {
+            Auth.auth().createUser(withEmail: email, password: password){ (user, error) in
+         if error == nil {
+           self.performSegue(withIdentifier: "SelectCategoryPageViewController", sender: self)
+                        }
+         else{
+           let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+           let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                            
+            alertController.addAction(defaultAction)
+            self.present(alertController, animated: true, completion: nil)
+               }
+                    }
+              }
+    }
+    
+  
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
